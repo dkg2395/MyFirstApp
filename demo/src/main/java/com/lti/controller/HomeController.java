ae7a977cc6b9eb6ddf67608375dd23b19d3719a7
Administrator <Administrator@host.docker.internal>
2021-12-08 16:44:09 +0530

    first file done

---
+++ demo/src/main/java/com/lti/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.lti.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class HomeController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Welcome to lti";
+	}
+	
+	@GetMapping("/{id}")
+	public String pathVariable(@PathVariable Integer id) {
+		return "Welcome to lti"+id;
+	}
+
+
+}
